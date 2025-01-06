6bbbe14201333dfc7dc8a48c5b25797581ad7c30
Ayush Singh <asayushsingh80@gmail.com>
2025-01-07 01:22:55 +0530

    ui: background setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
+import Navbar from "./components/Navbar";
+import { BackgroundBoxes } from "./components/ui/background-boxes";
 
 function App() {
-  return <Button>yo</Button>;
+  return (
+    <>
+      <div className="absolute inset-0 sm:block overflow-hidden">
+        <BackgroundBoxes />
+      </div>
+      <main className="bg-red-500 z-10 max-w-xl mx-auto px-6 mt-6 relative">
+        <Navbar />
+        <section>intro</section>
+      </main>
+    </>
+  );
 }
 
 export default App;
