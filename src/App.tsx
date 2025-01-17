5a71e2c1c84a4912b1afc310fdb49c9ecf412739
Ayush Singh <asayushsingh80@gmail.com>
2025-01-18 02:58:48 +0530

    ui: entry animation for whole

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Experience from "./components/Experience";
 import Navbar from "./components/Navbar";
 import { ShootingStars } from "./components/ui/shooting-stars";
 import { StarsBackground } from "./components/ui/stars-background";
+import { motion } from "framer-motion";
 
 function App() {
   return (
@@ -13,12 +14,18 @@ function App() {
         <StarsBackground />
         <ShootingStars maxSpeed={10} starWidth={20} starHeight={10} />
       </div>
-      <main className="z-10 max-w-xl mx-auto px-6 mt-6 relative flex flex-col gap-8">
+      <motion.main
+        initial={{ opacity: 0, y: 80 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -80 }}
+        transition={{ duration: 0.5 }}
+        className="z-10 max-w-xl mx-auto px-6 mt-6 relative flex flex-col gap-8"
+      >
         <Navbar />
         <About />
         <Experience />
         <Education />
-      </main>
+      </motion.main>
     </>
   );
 }
