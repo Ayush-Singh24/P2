ab1f5beef40531fd831c2e57cc874294f3005298
Ayush Singh <asayushsingh80@gmail.com>
2025-01-17 20:06:21 +0530

    feat: star background and shooting stars

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
-import { BackgroundBoxes } from "./components/ui/background-boxes";
+import { ShootingStars } from "./components/ui/shooting-stars";
+import { StarsBackground } from "./components/ui/stars-background";
 
 function App() {
   return (
     <>
-      {/* <div className="absolute inset-0 sm:block overflow-hidden"> */}
-      {/*   <BackgroundBoxes /> */}
-      {/* </div> */}
+      <div className="absolute inset-0 sm:block overflow-hidden">
+        <StarsBackground />
+        <ShootingStars maxSpeed={10} starWidth={20} starHeight={10} />
+      </div>
       <main className="z-10 max-w-xl mx-auto px-6 mt-6 relative flex flex-col gap-8">
         <Navbar />
         <About />
